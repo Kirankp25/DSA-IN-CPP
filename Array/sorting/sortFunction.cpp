2125936872570252372539680114179d0c926bb7
Kirankp <jnvkirankp25522@gmail.com>
2022-05-03 18:01:09 +0530

    inbuilt function of sorting

---
+++ Array/sorting/sortFunction.cpp
@@ -0,0 +1,18 @@
+//inbuilt fuctions in cpp for sorting
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int arr[]={23,4,5,2,6,1,78,5,56,33,98,143};
+    int n=sizeof(arr)/sizeof(int);
+    cout<<"bsfore sorting"<<endl;
+    for(auto x:arr)
+    {cout<<x<<" ";}
+    cout<<endl;
+    sort(arr,arr+n);
+    cout<<"after sorting"<<endl;
+    for(auto x:arr)
+    {cout<<x<<" ";}
+    return 0;
+}
