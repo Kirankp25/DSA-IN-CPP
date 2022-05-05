4037ac4d7449de23b695b65dc94b3dad9afdc0a2
Kirankp <jnvkirankp25522@gmail.com>
2022-05-05 22:57:39 +0530

    update

---
+++ String/cingetLine.cpp
@@ -0,0 +1,13 @@
+//using cin.getline()function you can read data form user 
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char sentence[1000];
+    cin.getline(sentence,1000);
+    cout<<sentence<<endl;
+    cout<<strlen(sentence)<<endl;
+    cout<<sizeof(sentence)<<endl;
+    return 0;
+}
