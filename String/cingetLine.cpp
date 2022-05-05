e9f9362c9f59693ae15ffed9dae2985dca911e5b
Kirankp <jnvkirankp25522@gmail.com>
2022-05-05 23:06:46 +0530

    update

--- String/cingetLine.cpp
+++ String/cingetLine.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     char sentence[1000];
-    cin.getline(sentence,1000);
+    cin.getline(sentence,1000,'.');
     cout<<sentence<<endl;
     cout<<strlen(sentence)<<endl;
     cout<<sizeof(sentence)<<endl;
