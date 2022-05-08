1defba283e22f105ff1f504a3c6c949b2c1cc448
Kirankp <jnvkirankp25522@gmail.com>
2022-05-08 16:31:22 +0530

    update

--- String/inbuiltfun.cpp
+++ String/inbuiltfun.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 int main()
 {
-    char a[100]="kiran ";
-    char b[100]="patil ";
+    char a[100]="kiran";
+    char b[100]="patil";
 
     cout<<strlen(a)<<endl;
     //strcpy(b,a);
     cout<<b<<endl;
     cout<<a<<endl;
     cout<<strcmp(a,b)<<endl;
-    cout<<strcat(a,b)<<endl;
+    char web[]="www.";
+    char domain[]="codingminutes.com";
+
+    cout<<strcat(web,domain)<<endl;
    
    
 
