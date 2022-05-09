7bafc664112f605e2c2b0bc287379737a6db998e
Kirankp <jnvkirankp25522@gmail.com>
2022-05-09 18:04:09 +0530

    update

---
+++ Array/TwoDArray/spiral.cpp
@@ -0,0 +1,51 @@
+//print all spiral element of given matrix
+#include<iostream>
+using namespace std;
+void print(int arr[][100],int n,int m)
+{
+    int sr=0;
+    int er=n-1;
+    int sc=0;
+    int ec=m-1;
+    while(sc<=ec && sr<=er)
+    {
+        int r,c;
+        for(int c = sc ;c<=ec; c++)//start row
+        {
+            cout<<arr[sr][c]<<" ";
+        }
+        for(int r = sr + 1 ;r<=er; r++)//end col
+        {
+            cout<<arr[r][ec]<<" ";
+        }
+        for(int c = ec - 1 ;c>=sc; c--)//end row
+        {
+            cout<<arr[er][c]<<" ";
+        }
+        for(int r = er - 1 ;r>=sr+1; r--)//start col
+        {
+            if(sc==ec)
+            {
+                break;
+            }
+            cout<<arr[r][sc]<<" ";
+        }
+        sr++;
+        er--;
+        sc++;
+        ec--;
+        
+    }
+}
+int main()
+{
+    
+    int arr[][100]={{1,2,3,4},
+                   {5,6,7,8},
+                   {9,10,11,12},
+                   {13,14,15,16}};
+    int n=4,m=4;
+    print(arr,n,m);
+    return 0;
+}
+    
