8efe0beb9409ac6af9569c5dac3a29e03987e875
Kirankp <jnvkirankp25522@gmail.com>
2022-05-17 15:52:56 +0530

    update

---
+++ vector/range.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{
+    int n;
+    cin>>n;
+    if(n&1)
+    return true;
+    while(n--)
+    {
+        
+    }
+    return 0;
+}
---
+++ vector/zerocount.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+int zerocount(int n)
+{
+    while(n--)
+    {
+        
+    }
+}
+
+
+int main()
+{
+    int  n;
+    std::cin>>n;
+    int x=isoddeven(n);
+    if(x==1)
+    std::cout<<"odd";
+    if(x==0)
+    std::cout<<"even";
+    return 0;
+}
