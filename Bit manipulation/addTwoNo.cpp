ba209199f8263d53b1cf42e4f846a4cbfc2befc9
Kirankp <jnvkirankp25522@gmail.com>
2022-05-22 01:38:33 +0530

    update

---
+++ Bit manipulation/addTwoNo.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+/*void ithbit(int &n,int p)
+{
+    int mask=n>>p;
+    n=(mask&1) > 0 ? 1:0;
+    
+}*/
+void equal()
+string add(string n,string p)
+{
+    
+
+}
+int main()
+{                           //13=1101
+    string n="11";                
+    string p="1";
+    //std::cin>>p;
+    //ithbit(n,p);
+    //std::cout<<n;
+    return 0;
+}
