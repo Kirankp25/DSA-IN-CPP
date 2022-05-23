b752d7773fd1373e2e9b1a1edabcdf952f7cf9e4
Kirankp <jnvkirankp25522@gmail.com>
2022-05-23 23:40:23 +0530

    update

--- Array/sorting/merge1.cpp
+++ Array/sorting/merge1.cpp
@@ -1,19 +1,12 @@
 #include<iostream>
 using namespace std;
-void merge(int arr[],int s,int e)
-{
-    if(s>=e)
-    return;
-    if(s<=e)
-
-}
 int main()
 {
     int arr[]={4,3,6,2,8,4,9,6,3,0};
     int s=0;
     int e=sizeof(arr)/sizeof(int);
-    //cout<<e<<endl;
-    cout<<merge(arr,s,e);
+    cout<<e<<endl;
+    //cout<<merge(arr,s,e);
     
     return 0;
 }
