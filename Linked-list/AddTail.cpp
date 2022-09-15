dacae29852b23f0c3eaf96a36a49c4db3a8ce39f
Kirankp <jnvkirankp25522@gmail.com>
2022-09-15 19:36:45 +0530

    update

--- Linked-list/AddTail.cpp
+++ Linked-list/AddTail.cpp
@@ -12,6 +12,14 @@ class node{
             next=NULL;
         }
 };
+void insertAt_Head(node* &head,int val)
+{
+    node* n=new node(val);
+    n->next=head;
+    head=n;
+}
+
+//insert element at the end of linked list
 void tail(node* &head, int val){
     
     node* n=new node(val);
@@ -28,10 +36,26 @@ void tail(node* &head, int val){
 
 
 }
+
+//deletion function
+void deletion(node* &head, int val)
+{
+    node* temp=head;
+    while(temp->next->data!=val){
+        temp=temp->next;
+    }
+    node* todelete=temp->next;
+    temp->next=temp->next->next;
+
+    delete todelete;
+}
+
+
+//display all element present in the linked list
 void display(node* head){
     node* temp=head;
     while(temp!=NULL){
-        cout<<temp->data<<"->";
+        cout<<temp->data<<" -> ";
         temp=temp->next;
 
     }
@@ -43,6 +67,11 @@ int main(){
     tail(head,1);
     tail(head,2);
     tail(head,3);
+    display(head);
+    deletion(head,3);
+    insertAt_Head(head,0);
+    insertAt_Head(head,-1);
+    insertAt_Head(head,-2);
     display(head);
 
     return 0;
