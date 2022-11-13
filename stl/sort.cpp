ca0856e893ced4fd9dedc9d8f0401c38f2f6159e
Kirankp <jnvkirankp25522@gmail.com>
2022-11-13 23:59:00 +0530

    update

--- stl/sort.cpp
+++ stl/sort.cpp
@@ -12,6 +12,12 @@ int main() {
     
     for(auto x:arr)
     cout<<x<<" ";
+    cout<<endl;
+    vector<int> vec = {4,2,1};
+
+    sort(vec.begin(), vec.end());
+    
+    cout<<vec[0]<<" "<<vec[1]<<" "<<vec[2];
     
     return 0;
 }
