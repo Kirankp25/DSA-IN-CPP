da63dccc00bf4414c7c986b3be6795c1339b581c
Kirankp <jnvkirankp25522@gmail.com>
2022-12-27 01:34:14 +0530

    initial commit

---
+++ Pattern/basic1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int c=10;
+    cout<<c++<<" "<<c--<<" "<<++c<<" "<<++c<<" "<<c;
+                       //  9 10 10 9 10
+    return 0;
+}
