89d7f67745d956647fd39d5f33b97d28489ce503
Kirankp <jnvkirankp25522@gmail.com>
2023-02-13 22:03:15 +0530

    update

--- Pattern/tempCodeRunnerFile.cpp
+++ Pattern/tempCodeRunnerFile.cpp
@@ -1,15 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=6;i>0;i--)
-    {
-        for(int j=1;j<=i;j++)
-        {
-            cout<<" "<<"*";
-        }
-        cout<<endl;
-    }
-    for(int i=6;i>0;i--)
+    for(int i=1;i<=6;i++)
     {
         for(int j=1;j<=i;j++)
         {
